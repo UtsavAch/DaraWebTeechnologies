07065a7e8d3abf472688caab5a75e4d90bebaedc
s-virag <virag.saghi@edu.bme.hu>
2024-10-25 18:00:22 +0100

    Finalize the gamemode screens

--- config.js
+++ config.js
@@ -3,25 +3,54 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const multiPlayerButton = document.getElementById('multiplayer-btn');
     const singlePlayerView = document.getElementById('singleplayer-view');
     const multiPlayerView = document.getElementById('multiplayer-view');    
+    const loginView = document.getElementById('login-view');
+    const loginButton = document.getElementById('login-btn');
+    const registerButton = document.getElementById('register-btn');
+    const loginForm = document.getElementById('login-form');
+    const registerForm = document.getElementById('register-form');
+    const loginSubmit = document.getElementById('login-submit');
+    const registerSubmit = document.getElementById('register-submit');
 
-    if (!singlePlayerButton || !multiPlayerButton || !singlePlayerView) {
-        console.error('One or more elements are missing');
-        return;
-    }
 
     singlePlayerButton.addEventListener('click', () => {
-        console.log('Single Player Button Clicked');
         singlePlayerView.style.display = 'block';
         multiPlayerView.style.display = 'none';
         multiPlayerButton.classList.add('passive-mode');
         singlePlayerButton.classList.remove('passive-mode');
+
+        loginView.style.display = 'none';
     });
 
     multiPlayerButton.addEventListener('click', () => {
-        console.log('Multi Player Button Clicked');
         singlePlayerView.style.display = 'none';
-        multiPlayerView.style.display = 'block';
+        multiPlayerView.style.display = 'none';
         multiPlayerButton.classList.remove('passive-mode');
         singlePlayerButton.classList.add('passive-mode');
+        loginView.style.display = 'block';
     });
+
+    loginButton.addEventListener('click', () => {
+        registerForm.style.display = 'none';
+        loginForm.style.display = 'block';
+    });
+
+    registerButton.addEventListener('click', () => {
+        loginForm.style.display = 'none';
+        registerForm.style.display = 'block';
+    });
+
+    loginSubmit.addEventListener('click', () => {
+        //no validation for now
+        loginForm.style.display = 'none';
+        loginView.style.display = 'none';
+        multiPlayerView.style.display = 'block';
+    });
+
+    registerSubmit.addEventListener('click', () => {
+        //no validation for now
+        registerForm.style.display = 'none';
+        loginView.style.display = 'none';
+        multiPlayerView.style.display = 'block';
+    });
+
 });
