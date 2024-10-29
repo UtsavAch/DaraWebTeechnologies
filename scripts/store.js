bfd90e8630bca3b78d94f78c9d1479ec12117f07
UtsavAch <acharya.utsav.15@gmail.com>
2024-10-29 11:30:06 +0000

    Store structure to store data

---
+++ scripts/store.js
@@ -0,0 +1,24 @@
+registered_players = [
+  {
+    id: 1,
+    username: "andre",
+    password: "andre123",
+    total_wins: 5,
+  },
+
+  {
+    id: 2,
+    username: "utsav",
+    password: "utsav123",
+    total_wins: 3,
+  },
+
+  {
+    id: 3,
+    username: "virag",
+    password: "virag123",
+    total_wins: 4,
+  },
+
+  // Will get more players from the form
+];
