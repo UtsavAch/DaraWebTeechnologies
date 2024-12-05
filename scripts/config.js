01351eb94402f7f10e296233e253b67efb0df8aa
s-virag <virag.saghi@edu.bme.hu>
2024-12-05 15:41:35 +0000

    Change multiplayer frontend

--- scripts/config.js
+++ scripts/config.js
@@ -2,39 +2,45 @@ document.addEventListener("DOMContentLoaded", (event) => {
   const singlePlayerButton = document.getElementById("singleplayer-btn");
   const multiPlayerButton = document.getElementById("multiplayer-btn");
   const singlePlayerView = document.getElementById("singleplayer-view");
-  const multiPlayerView = document.getElementById("multiplayer-view");
+  //const multiPlayerView = document.getElementById("multiplayer-view");
   const loginView = document.getElementById("login-view");
-  const loginButton = document.getElementById("login-btn");
+  //const loginButton = document.getElementById("login-btn");
   const registerButton = document.getElementById("register-btn");
   const loginForm = document.getElementById("login-form");
   const registerForm = document.getElementById("register-form");
   const loginSubmit = document.getElementById("login-submit");
+  const registerQuestion = document.getElementById("register-question");
   const registerSubmit = document.getElementById("register-submit");
+  const loginLink = document.getElementById("login-link");
 
   singlePlayerButton.addEventListener("click", () => {
     singlePlayerView.style.display = "block";
-    multiPlayerView.style.display = "none";
+    //multiPlayerView.style.display = "none";
     multiPlayerButton.classList.add("passive-mode");
     singlePlayerButton.classList.remove("passive-mode");
 
     loginView.style.display = "none";
+    registerForm.style.display = "none";
   });
 
   multiPlayerButton.addEventListener("click", () => {
     singlePlayerView.style.display = "none";
-    multiPlayerView.style.display = "none";
+    //multiPlayerView.style.display = "none";
     multiPlayerButton.classList.remove("passive-mode");
     singlePlayerButton.classList.add("passive-mode");
     loginView.style.display = "block";
+    loginForm.style.display = "block";
+    registerQuestion.style.display = "block";
   });
 
-  loginButton.addEventListener("click", () => {
+/*  loginButton.addEventListener("click", () => {
     registerForm.style.display = "none";
     loginForm.style.display = "block";
-  });
+  });*/
 
   registerButton.addEventListener("click", () => {
     loginForm.style.display = "none";
+    registerQuestion.style.display = "none";
     registerForm.style.display = "block";
   });
 
@@ -42,14 +48,20 @@ document.addEventListener("DOMContentLoaded", (event) => {
     //no validation for now
     loginForm.style.display = "none";
     loginView.style.display = "none";
-    multiPlayerView.style.display = "block";
+    //multiPlayerView.style.display = "block";
   });
 
   registerSubmit.addEventListener("click", () => {
     //no validation for now
     registerForm.style.display = "none";
-    loginView.style.display = "none";
-    multiPlayerView.style.display = "block";
+    loginForm.style.display = "block";
+    registerQuestion.style.display = "block";
+  });
+
+  loginLink.addEventListener("click", () => {
+    loginForm.style.display = "block";
+    registerForm.style.display = "none";
+    registerQuestion.style.display = "block";
   });
 });
 
